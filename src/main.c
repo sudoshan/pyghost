8cf2952528ec30f763b5cffc44ae380f1da49f2c
sudoshan <sudoishan@gmail.com>
2025-01-18 14:57:37 +0545

    implement previous configuration

--- src/main.c
+++ src/main.c
@@ -1,71 +1,132 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
 
 // reads the size of the file given
-uint64_t colfilesize(FILE *colorfile) {
-  if (colorfile != NULL) {
-    if (fseek(colorfile, 0, SEEK_END) != 0) {
-      fprintf(stderr, "error");
+uint64_t filesize(FILE *file) {
+  if (file != NULL) {
+    if (fseek(file, 0, SEEK_END) != 0) {
+      printf("error\n");
       return 0;
     }
-    uint64_t size = ftell(colorfile);
-    if (fseek(colorfile, 0, SEEK_SET) != 0) {
-      fprintf(stderr, "error");
+    uint64_t size = ftell(file);
+    if (fseek(file, 0, SEEK_SET) != 0) {
+      printf("error\n");
       return 0;
     }
     return size;
   }
+
+  else {
+    printf("not able to read size of color file\n");
+  }
 }
 
-// reads the colors (contents) of the file as per the given directory
-char **readcolors(uint64_t size, FILE *colorfile, int linenum) {
-  char line[linenum];
-  char **colors = malloc(size);
+// reads the number of lines in the file
+uint64_t numlines(FILE *file) {
+  uint64_t lines = 0;
+
+  while (!feof(file)) {
+    if (feof(file)) {
+      break;
+    }
+
+    char ch = fgetc(file);
+    if (ch == '\n') {
+      lines++;
+    }
+  }
+
+  fseek(file, 0, SEEK_SET);
+  return lines;
+}
+
+// reads the contents of the file as per the given directory
+char **readfile(uint64_t size, FILE *file, int numlines) {
+  char buffline[100];
+  char **buffer = malloc(size);
+
   int index = 0;
 
-  if (colorfile != NULL) {
-    while (fgets(line, linenum, colorfile)) {
-      colors[index] = malloc(size);
-      strcpy(colors[index], line);
+  if (file != NULL) {
+    while (fgets(buffline, numlines, file)) {
+      buffer[index] = malloc(100);
+      strcpy(buffer[index], buffline);
       index++;
     }
 
-    return colors;
+    return buffer;
   }
 
   else {
-    printf("not able to open file\n");
+    printf("not able to open colors file\n");
+  }
+
+  free(buffer);
+}
+
+int checker(FILE *readconf) {
+  char line[100];
+  int count = 1;
+  fseek(readconf, 0, SEEK_SET);
+  
+  if (readconf != NULL) {
+    while (fgets(line, 100, readconf)) {
+      if (strstr(line, "palette")) {
+        break;
+      }
+      count++;
+    }
+    
+    return count;
   }
 }
 
-// appends the ghostty config file
-void append(FILE *config, char **colors, int linenum) {
-  char string[] = "palette"; // searches for initial palette definitions
+// rewrite the ghostty config file
+void rewrite(FILE *confile, char **conf, int count, char **colors, int colorlines) {
+  if (confile != NULL) {
+    for (int i = 0; i <= count + 1; i++) {
+      fprintf(confile, "%s", conf[i]);
+    }
+
+    for (int i = 0; i < colorlines; i++) {
+      fprintf(confile, "palette = %d=%s", i, colors[i]);
+    }
   
-  for (int i = 0; i < linenum; i++) {
-    fprintf(config, "palette = %d=%s", i, colors[i]);
+    fprintf(confile, "background = %s", colors[0]);
+    fprintf(confile, "foreground = %s", colors[15]);
   }
-  
-  fprintf(config, "background = %s", colors[0]);
-  fprintf(config, "foreground = %s", colors[15]);
+
+  else {
+    printf("not able to open ghostty config file\n");
+  }
 }
 
 void main() {
   char *dircolor = "/home/ishan/.cache/wal/colors";
-  char *dirconfig = "/home/ishan/.config/ghostty/config";
+  char *dirconf = "/home/ishan/.config/ghostty/config";
 
   FILE *colorfile = fopen(dircolor, "r"); 
-  FILE *config = fopen(dirconfig, "a");
 
-  uint64_t size = colfilesize(colorfile); 
-  int linenum = size / 8; 
+  uint64_t colorsize = filesize(colorfile); 
+  uint64_t colorlines = numlines(colorfile); 
+  char **colors = readfile(colorsize, colorfile, colorlines); 
 
-  char **colors = readcolors(size, colorfile, linenum); 
+  FILE *readconf = fopen(dirconf, "r");
 
-  append(config, colors, linenum);
+  uint64_t confsize = filesize(readconf);
+  uint64_t conflines = numlines(readconf); 
+  char **conf = readfile(confsize, readconf, conflines);
+
+  int count = checker(readconf);
+
+  fclose(readconf);
+
+  FILE *confile = fopen(dirconf, "w");
+  rewrite(confile, conf, count, colors, colorlines);
 
   fclose(colorfile);
-  fclose(config);
+  fclose(confile);
 }
