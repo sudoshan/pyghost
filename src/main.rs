295c2ea16cfd7a347ad7819cabc00e6752259acb
Kat <65649991+00-kat@users.noreply.github.com>
2025-01-24 21:49:55 +1100

    Rewrite it in Rust.
    Added features:
      - Memory safety¹.
      - Conforming to the XDG Base Directory Specification if the
        corresponding variables are set².
      - Removing any entry for `theme` from the Ghostty configuration.
    
    Features not added yet, but to be added in a future commit:
      - Support for reading and writing the Ghostty configuration from the
        Application Support directory on macOS.
    
    Removed features:
      - The ability to compile and run Walors on systems which are not
        supported by Rust.  This is not actually an issue as Ghostty is not
        supported on those platforms anyway.
    
    ¹Before the rewrite, I was facing a segmentation fault due to an
    out-of-bounds array access in the `line` array in `readoptions()`.  This
    rewrite fixes it, as well as any other memory safety issues that may
    have been lurking in the original C implementation.
    
    ²I am unsure if Pywal itself supports the XDG Base Directory
    Specification or not.  A peek at the source code suggests that it in
    fact has support for it, but trying to set $XDG_CACHE_HOME or
    $XDG_CONFIG_HOME did not work.

---
+++ src/main.rs
@@ -0,0 +1,81 @@
+use std::process::ExitCode;
+use std::{env, fs};
+
+fn color_folder(home: &str) -> String {
+    match env::var("XDG_CACHE_HOME") {
+        Ok(cache_dir) => format!("{cache_dir}/wal/colors"),
+        Err(_) => format!("{home}/.cache/wal/colors"),
+    }
+}
+
+fn config_folder(home: &str) -> String {
+    match env::var("XDG_CONFIG_HOME") {
+        Ok(conf_dir) => format!("{conf_dir}/ghostty/config"),
+        Err(_) => format!("{home}/.config/ghostty/config"),
+    }
+}
+
+fn read_options(conf: &str) -> Vec<&str> {
+    conf.lines()
+        .filter(|line| {
+            line.split_once('=')
+                .and_then(|(k, _)| {
+                    Some(!matches!(
+                        k.trim(),
+                        "theme" | "palette" | "background" | "foreground",
+                    ))
+                })
+                .unwrap_or(true)
+        })
+        .collect()
+}
+
+fn get_new_config(colors: Vec<&str>, old_config: Vec<&str>) -> String {
+    old_config.join("\n")
+        + "\n"
+        + &colors
+            .iter()
+            .enumerate()
+            .map(|(i, c)| format!("palette = {i}={c}"))
+            .fold(String::new(), |mut acc: String, s: String| -> String {
+                acc.reserve(s.len() + 1);
+                acc.push_str(&s);
+                acc.push('\n');
+                acc
+            })
+        + &format!(
+            "background = {}\n\
+             foreground = {}\n",
+            colors[0],
+            colors[colors.len() - 1]
+        )
+}
+
+fn main() -> ExitCode {
+    let Ok(home) = env::var("HOME") else {
+        eprintln!("error, home");
+        return ExitCode::FAILURE;
+    };
+    let color_dir = color_folder(&home);
+    let conf_dir = config_folder(&home);
+
+    let Ok(colors) = fs::read_to_string(&color_dir) else {
+        eprintln!("error, opening {color_dir}");
+        return ExitCode::FAILURE;
+    };
+    let colors: Vec<&str> = colors.lines().collect();
+
+    let Ok(conf) = fs::read_to_string(&conf_dir) else {
+        eprintln!("error, opening {conf_dir}");
+        return ExitCode::FAILURE;
+    };
+
+    let options = read_options(&conf);
+
+    if let Err(_) = fs::write(&conf_dir, get_new_config(colors, options)) {
+        eprintln!("error, writing {conf_dir}");
+        return ExitCode::FAILURE;
+    };
+
+    ExitCode::SUCCESS
+}
